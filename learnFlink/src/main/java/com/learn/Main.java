fa795245886d1f991486736d38c0475cd8c2888a
mazhuang <mazhuang1995@yeah.net>
2023-07-02 18:08:22 +0800

    窗口学习-20230702

--- learnFlink/src/main/java/com/learn/Main.java
+++ learnFlink/src/main/java/com/learn/Main.java
@@ -3,7 +3,6 @@ package com.learn;
 public class Main {
     public static void main(String[] args) {
 //测试
-//        System.out.println("Hello world!");
         System.out.println("Hello world!");
     }
 }
